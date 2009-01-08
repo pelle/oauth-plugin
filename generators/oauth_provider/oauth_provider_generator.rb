26a092d665bcade82d243f0a260a0ca5e10fa7d1
Ed Hickey <ed@ed-new-laptop.local>
2009-01-08 12:19:49 -0600

    forgot to commit these in last commit

--- generators/oauth_provider/oauth_provider_generator.rb
+++ generators/oauth_provider/oauth_provider_generator.rb
@@ -38,10 +38,6 @@ class OauthProviderGenerator < Rails::Generator::Base
       m.directory File.join('app/helpers', controller_class_path)
       m.directory File.join('app/views', controller_class_path, controller_file_name)
       m.directory File.join('app/views', controller_class_path, 'oauth_clients')
-      m.directory File.join('spec')
-      m.directory File.join('spec/models')
-      m.directory File.join('spec/fixtures', class_path)
-      m.directory File.join('spec/controllers', controller_class_path)
 
       m.template 'client_application.rb',File.join('app/models',"client_application.rb")
       m.template 'oauth_token.rb',    File.join('app/models',"oauth_token.rb")
@@ -49,23 +45,42 @@ class OauthProviderGenerator < Rails::Generator::Base
       m.template 'access_token.rb',   File.join('app/models',"access_token.rb")
       m.template 'oauth_nonce.rb',    File.join('app/models',"oauth_nonce.rb")
 
-      m.template 'client_application_spec.rb',File.join('spec/models',"client_application_spec.rb")
-      m.template 'oauth_token_spec.rb',    File.join('spec/models',"oauth_token_spec.rb")
-      m.template 'oauth_nonce_spec.rb',    File.join('spec/models',"oauth_nonce_spec.rb")
-
-
-      m.template 'client_applications.yml',File.join('spec/fixtures',"client_applications.yml")
-      m.template 'oauth_tokens.yml',    File.join('spec/fixtures',"oauth_tokens.yml")
-      m.template 'oauth_nonces.yml',    File.join('spec/fixtures',"oauth_nonces.yml")
-      
       m.template 'controller.rb',File.join('app/controllers',controller_class_path,"#{controller_file_name}_controller.rb")
       m.template 'helper.rb',File.join('app/helpers',controller_class_path,"#{controller_file_name}_helper.rb")
-      m.template 'controller_spec_helper.rb', File.join('spec/controllers', controller_class_path,"#{controller_file_name}_controller_spec_helper.rb")
-      m.template 'controller_spec.rb',File.join('spec/controllers',controller_class_path,"#{controller_file_name}_controller_spec.rb")
-      
+
       m.template 'clients_controller.rb',File.join('app/controllers',controller_class_path,"#{controller_file_name}_clients_controller.rb")
       m.template 'clients_helper.rb',File.join('app/helpers',controller_class_path,"#{controller_file_name}_clients_helper.rb")
-      m.template 'clients_controller_spec.rb',File.join('spec/controllers',controller_class_path,"#{controller_file_name}_clients_controller_spec.rb")
+
+      if !options[:test_unit]
+        m.directory File.join('spec')
+        m.directory File.join('spec/models')
+        m.directory File.join('spec/fixtures', class_path)
+        m.directory File.join('spec/controllers', controller_class_path)
+        
+        m.template 'client_application_spec.rb',File.join('spec/models',"client_application_spec.rb")
+        m.template 'oauth_token_spec.rb',    File.join('spec/models',"oauth_token_spec.rb")
+        m.template 'oauth_nonce_spec.rb',    File.join('test/unit',"oauth_nonce_spec.rb")
+        m.template 'client_applications.yml',File.join('spec/fixtures',"client_applications.yml")
+        m.template 'oauth_tokens.yml',    File.join('spec/fixtures',"oauth_tokens.yml")
+        m.template 'oauth_nonces.yml',    File.join('spec/fixtures',"oauth_nonces.yml")
+        m.template 'controller_spec_helper.rb', File.join('spec/controllers', controller_class_path,"#{controller_file_name}_controller_spec_helper.rb")
+        m.template 'controller_spec.rb',File.join('spec/controllers',controller_class_path,"#{controller_file_name}_controller_spec.rb")      
+        m.template 'clients_controller_spec.rb',File.join('spec/controllers',controller_class_path,"#{controller_file_name}_clients_controller_spec.rb")
+      else
+        m.directory File.join('test')
+        m.directory File.join('test/unit')
+        m.directory File.join('test/fixtures', class_path)
+        m.directory File.join('test/functional', controller_class_path)
+        m.template 'client_application_test.rb',File.join('test/unit',"client_application_test.rb")
+        m.template 'oauth_token_test.rb',    File.join('test/unit',"oauth_token_test.rb")
+        m.template 'oauth_nonce_test.rb',    File.join('test/unit',"oauth_nonce_test.rb")
+        m.template 'client_applications.yml',File.join('test/fixtures',"client_applications.yml")
+        m.template 'oauth_tokens.yml',    File.join('test/fixtures',"oauth_tokens.yml")
+        m.template 'oauth_nonces.yml',    File.join('test/fixtures',"oauth_nonces.yml")
+        m.template 'controller_test_helper.rb', File.join('test', controller_class_path,"#{controller_file_name}_controller_test_helper.rb")
+        m.template 'controller_test.rb',File.join('test/functional',controller_class_path,"#{controller_file_name}_controller_test.rb")
+        m.template 'clients_controller_test.rb',File.join('test/functional',controller_class_path,"#{controller_file_name}_clients_controller_test.rb")
+      end
 
       m.template '_form.html.erb',  File.join('app/views', controller_class_path, 'oauth_clients', "_form.html.erb")
       m.template 'new.html.erb',  File.join('app/views', controller_class_path, 'oauth_clients', "new.html.erb")
@@ -94,5 +109,7 @@ class OauthProviderGenerator < Rails::Generator::Base
       opt.separator 'Options:'
       opt.on("--skip-migration", 
              "Don't generate a migration file") { |v| options[:skip_migration] = v }
+      opt.on("--test-unit", 
+             "Generate the Test::Unit compatible tests instead of RSpec") { |v| options[:test_unit] = v }
     end
 end
