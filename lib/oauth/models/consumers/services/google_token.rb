aaad6f6c28ae7d43f77ee99e85cb625282471f67
Pelle Braendgaard <pelleb@gmail.com>
2009-10-05 17:48:59 -0400

    GoogleToken now uses the Portable Contact gem

--- lib/oauth/models/consumers/services/google_token.rb
+++ lib/oauth/models/consumers/services/google_token.rb
@@ -1,3 +1,5 @@
+require 'portablecontacts'
+
 class GoogleToken < ConsumerToken
   GOOGLE_SETTINGS={
     :site=>"https://www.google.com", 
@@ -13,29 +15,13 @@ class GoogleToken < ConsumerToken
   def self.create_consumer(options={})
     OAuth::Consumer.new credentials[:key],credentials[:secret],GOOGLE_SETTINGS.merge(options)
   end
-  
-  def self.portable_contacts_consumer
-    @portable_contacts_consumer||= create_consumer :site=>"http://www-opensocial.googleusercontent.com"
-  end
-  
-  
+    
   def self.get_request_token(callback_url, scope=nil)
     consumer.get_request_token({:oauth_callback=>callback_url}, :scope=>scope||credentials[:scope]||"http://www-opensocial.googleusercontent.com/api/people")
   end
   
   def portable_contacts
-    @portable_contacts||= GooglePortableContacts.new(OAuth::AccessToken.new( self.class.portable_contacts_consumer, token, secret))
+    @portable_contacts||= PortableContacts::Client.new "http://www-opensocial.googleusercontent.com/api/people", client
   end
-  
-  class GooglePortableContacts < Oauth::Models::Consumers::SimpleClient
     
-    def me
-      get("/api/people/@me/@self")
-    end
-
-    def all
-      get("/api/people/@me/@all")
-    end
-
-  end
 end
