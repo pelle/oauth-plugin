ec4efb69f6f4925a7f891840c0d3faf7ae930edc
Pelle Braendgaard <pelleb@gmail.com>
2010-04-30 14:56:31 -0400

    disable autoloading in rails 3

--- rails/init.rb
+++ rails/init.rb
@@ -1,7 +1,9 @@
-gem 'oauth', '>=0.3.5'
-require 'oauth/signature/hmac/sha1'
-require 'oauth/request_proxy/action_controller_request'
-require 'oauth/server'
-require 'oauth/controllers/application_controller_methods'
+if Rails.version =~ /^2\./
+  gem 'oauth', '>=0.3.5'
+  require 'oauth/signature/hmac/sha1'
+  require 'oauth/request_proxy/action_controller_request'
+  require 'oauth/server'
+  require 'oauth/controllers/application_controller_methods'
 
-ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
+  ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
+end
