914becebfbab302ad16dead6d784ed04aa0e2045
Pelle Braendgaard <pelleb@gmail.com>
2010-05-06 13:33:28 -0400

    fixed to work with rails3

--- rails/init.rb
+++ rails/init.rb
@@ -1,9 +1,11 @@
-if Rails.version =~ /^2\./
-  gem 'oauth', '>=0.3.5'
-  require 'oauth/signature/hmac/sha1'
+require 'oauth'
+require 'oauth/signature/hmac/sha1'
+if Rails.version =~ /^3\./
+  require 'oauth/request_proxy/rack_request'
+else
   require 'oauth/request_proxy/action_controller_request'
-  require 'oauth/server'
-  require 'oauth/controllers/application_controller_methods'
+end
+require 'oauth/server'
+require 'oauth/controllers/application_controller_methods'
 
   ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
-end
