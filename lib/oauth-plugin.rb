cade84b8fee806cbf80aace5e2c65596945b3fc2
Pelle Braendgaard <pelle@stakeventures.com>
2010-12-03 17:10:09 -0500

    cleaned up the loading of the plugin

--- lib/oauth-plugin.rb
+++ lib/oauth-plugin.rb
@@ -1,9 +1,14 @@
 require 'oauth'
 require 'oauth/signature/hmac/sha1'
-require 'oauth/rack/oauth_filter'
 require 'oauth/request_proxy/rack_request'
 require 'oauth/server'
 require 'oauth/controllers/application_controller_methods'
+if Rails.version =~ /^3\./
+  require 'oauth/request_proxy/rack_request'
+else
+  require 'oauth/request_proxy/action_controller_request'
+  ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
+end
 
 
 module OAuth
--- rails/init.rb
+++ rails/init.rb
@@ -1,9 +1,1 @@
-require 'oauth'
-require 'oauth/signature/hmac/sha1'
-require 'oauth/rack/oauth_filter'
-require 'oauth/server'
-require 'oauth/controllers/application_controller_methods'
-if Rails.version =~ /^2\./
-  require 'oauth/request_proxy/action_controller_request'
-  ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
-end
+require 'oauth-plugin'
