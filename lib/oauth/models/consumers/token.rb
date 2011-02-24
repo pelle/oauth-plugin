e25f3f3fc447119d6d41ce8ae51bddf0d0a7886b
Aidan Feldman <aidan@jux.com>
2011-02-24 23:44:15 +0000

    let ActiveRecord handle setting of type for ConsumerTokens

--- lib/oauth/models/consumers/token.rb
+++ lib/oauth/models/consumers/token.rb
@@ -40,12 +40,17 @@ module Oauth
           
           def find_or_create_from_access_token(user,access_token)
             if user
-              user.consumer_tokens.first(:conditions=>{:type=>self.to_s,:token=>access_token.token}) ||
-                user.consumer_tokens.create!(:type=>self.to_s,:token=>access_token.token, :secret=>access_token.secret)
+              token = self.find_or_create_by_user_id_and_token(user.id, access_token.token)
             else
-              ConsumerToken.first( :conditions =>{ :token=>access_token.token,:type=>self.to_s}) ||
-                create(:type=>self.to_s,:token=>access_token.token, :secret=>access_token.secret)
+              token = self.find_or_create_by_token(access_token.token)
             end
+            
+            if token.new_record?
+              token.secret = access_token.secret
+              token.save
+            end
+
+            token
           end
           
           def build_user_from_token
