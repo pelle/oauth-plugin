caeeb01874c0cb3231af79f751cf9e139f9516b9
Aidan Feldman <aidan@jux.com>
2011-04-05 19:16:17 -0400

    connect to twitter with more generic token

--- lib/oauth/models/consumers/services/twitter_token.rb
+++ lib/oauth/models/consumers/services/twitter_token.rb
@@ -1,11 +1,12 @@
-require 'twitter'
 class TwitterToken < ConsumerToken
-  TWITTER_SETTINGS={:site=>"http://twitter.com"}
-  def self.consumer
-    @consumer||=OAuth::Consumer.new credentials[:key],credentials[:secret],TWITTER_SETTINGS
-  end
+  TWITTER_SETTINGS={
+    :site => "https://api.twitter.com",
+    :request_token_path => "/oauth/request_token",
+    :authorize_path => "/oauth/authorize",
+    :access_token_path => "/oauth/access_token",
+  }
   
-  def client
-    @client ||= Twitter::Client.new(:consumer_key => TwitterToken.consumer.key, :consumer_secret => TwitterToken.consumer.secret)
+  def self.consumer(options={})
+    @consumer ||= OAuth::Consumer.new(credentials[:key], credentials[:secret], TWITTER_SETTINGS.merge(options))
   end
 end
