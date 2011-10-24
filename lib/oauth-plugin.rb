26265aa0798a78df88683aa43731111d1caa26fe
chrisrhoden <carhoden@gmail.com>
2011-10-24 16:31:32 -0400

    Rails 2 compatibility

--- lib/oauth-plugin.rb
+++ lib/oauth-plugin.rb
@@ -5,18 +5,16 @@ require 'oauth/server'
 require 'oauth/controllers/application_controller_methods'
 if Rails.version =~ /^3\./
   require 'oauth/request_proxy/rack_request'
-else
-  require 'oauth/request_proxy/action_controller_request'
-  ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
-end
-
-
-module OAuth
-  module Plugin
-    class OAuthRailtie < Rails::Railtie
-      initializer "oauth-plugin.configure_rails_initialization" do |app|
-        ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
+  module OAuth
+    module Plugin
+      class OAuthRailtie < Rails::Railtie
+        initializer "oauth-plugin.configure_rails_initialization" do |app|
+          ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
+        end
       end
     end
   end
+else
+  require 'oauth/request_proxy/action_controller_request'
+  ActionController::Base.send :include, OAuth::Controllers::ApplicationControllerMethods
 end
