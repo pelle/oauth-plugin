6518d8af2e7ac2b8b92e5410cb38f855459993e8
Tys von Gaza <tys@tvg.ca>
2012-04-26 16:09:53 -0600

    update autoloader to load from rails first

--- lib/oauth/models/consumers/service_loader.rb
+++ lib/oauth/models/consumers/service_loader.rb
@@ -5,24 +5,31 @@ require 'oauth/controllers/consumer_controller'
 #
 # So an entry called "my_service" will create a class MyServiceToken which you can 
 # connect with has_one to your user model.
+
+# We use our own service loader to give priority to rails models
 if defined? ConsumerToken && defined? OAUTH_CREDENTIALS
   require File.join(File.dirname(__FILE__), 'services', 'oauth2_token')
 
   OAUTH_CREDENTIALS.each do |key, value|
     class_name=value[:class_name]||"#{key.to_s.classify}Token"
     unless Object.const_defined?(class_name.to_sym)
-      if File.exists?(File.join(File.dirname(__FILE__), "services","#{key.to_s}_token.rb"))
-        Rails.logger.info File.join(File.dirname(__FILE__), "services","#{key.to_s}_token")
-        require File.join(File.dirname(__FILE__), "services","#{key.to_s}_token")
-      else
-        begin
-          # Let Rails auto-load from the models folder
-          eval class_name
-        rescue NameError
+      # check if we have the file in our models folder
+      begin
+        # Let Rails auto-load from the models folder
+        eval class_name
+      rescue NameError
+        # next try to load from our services
+        if File.exists?(File.join(File.dirname(__FILE__), "services","#{key.to_s}_token.rb"))
+          Rails.logger.info File.join(File.dirname(__FILE__), "services","#{key.to_s}_token")
+          require File.join(File.dirname(__FILE__), "services","#{key.to_s}_token")
+        
+        # otherwise define the calss
+        else
           super_class = value[:super_class]||value[:oauth_version].to_i>=2 ? "Oauth2Token" : "ConsumerToken"
           eval "class #{class_name} < #{super_class} ;end"
         end
       end
+      
     end
   end
 end
