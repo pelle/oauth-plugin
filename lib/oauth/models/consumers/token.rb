36d2d67caedd0e5789503b80f793c1a5905d0031
Ruben Homs <rubenhoms@gmail.com>
2013-02-28 10:57:48 +0100

    Merged pull requests from https://github.com/pelle/oauth-plugin/pull/132 (OAuth 2 refresh tokens should now work) and https://github.com/pelle/oauth-plugin/pull/137 (allow? will now return false when the token is not found)

--- lib/oauth/models/consumers/token.rb
+++ lib/oauth/models/consumers/token.rb
@@ -39,14 +39,17 @@ module Oauth
           end
 
           def find_or_create_from_access_token(user,access_token)
-            secret = access_token.respond_to?(:secret) ? access_token.secret : nil
             if user
               token = self.find_or_initialize_by_user_id_and_token(user.id, access_token.token)
             else
               token = self.find_or_initialize_by_token(access_token.token)
             end
 
-            # set or update the secret
+            # set or update the secret and refresh_token
+            secret = access_token.respond_to?(:secret) ? access_token.secret : nil
+            refresh_token = access_token.respond_to?(:refresh_token) ? access_token.refresh_token : nil
+
+            token.refresh_token = refresh_token
             token.secret = secret
             token.save! if token.new_record? or token.changed?
 
