8e41129e4a1d8bedd8645e87d05c537ec4d193bf
Graham Davison <g.m.davison@computer.org>
2014-02-11 02:58:15 -0800

    add spec for ConsumerController#callback

--- spec/dummy_app/app/controllers/oauth_consumers_controller.rb
+++ spec/dummy_app/app/controllers/oauth_consumers_controller.rb
@@ -30,25 +30,17 @@ class OauthConsumersController < ApplicationController
     redirect_to root_url
   end
 
-  # The plugin requires logged_in? to return true or false if the user is logged in. Uncomment and
-  # call your auth frameworks equivalent below if different. eg. for devise:
-  #
-  # def logged_in?
-  #   user_signed_in?
-  # end
+  def logged_in?
+    current_user != nil
+  end
 
-  # The plugin requires current_user to return the current logged in user. Uncomment and
-  # call your auth frameworks equivalent below if different.
-  # def current_user
-  #   current_person
-  # end
+  def current_user
+    session[:user]
+  end
 
-  # The plugin requires a way to log a user in. Call your auth frameworks equivalent below
-  # if different. eg. for devise:
-  #
-  # def current_user=(user)
-  #   sign_in(user)
-  # end
+  def current_user=(user)
+    session[:user] = user
+  end
 
   # Override this to deny the user or redirect to a login screen depending on your framework and app
   # if different. eg. for devise:
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,5 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy_app/config/environment.rb",  __FILE__)
 
 Rails.backtrace_cleaner.remove_silencers!
+
+require 'rspec/rails'
