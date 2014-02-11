80bfaa7226f85f3c1c6aac2e5c8b549d04ad2088
Graham Davison <g.m.davison@computer.org>
2014-02-11 03:09:43 -0800

    add specs for ConsumerController#callback2

--- spec/controllers/consumer_controller_spec.rb
+++ spec/controllers/consumer_controller_spec.rb
@@ -54,4 +54,55 @@ describe OauthConsumersController do
       end
     end
   end
+  
+  describe "callback2" do
+    context 'when logged in' do
+      let(:user   ) { double("user", id: 1234) }
+      let(:service) { 'foo'           }
+      let(:token  ) { double("token") }
+      before(:each) do
+        session[:user] = user
+        session['my_token'] = 'my_secret'
+        ConsumerToken.should_receive(:find).and_return('a_token')
+        FooToken.should_receive(:access_token).and_return(token)
+        get :callback2, :id => service, :oauth_token => 'my_token'
+      end
+      it 'should redirect to root' do
+        response.should redirect_to root_url
+      end
+    end
+    context 'when not logged in' do
+      let(:user    ) { nil   }
+      let(:service ) { 'bar' }
+      let(:token   ) { double("token", user: new_user) }
+      let(:new_user) { Object.new }
+      before(:each) do
+        session[:user] = user
+        session['my_token'] = 'my_secret'
+        BarToken.should_receive(:access_token).and_return(token)
+        get :callback2, :id => service, :oauth_token => 'my_token'
+      end
+      it 'should set the current user' do
+        session[:user].should be(new_user)
+      end
+      it 'should redirect to root' do
+        response.should redirect_to root_url
+      end
+    end
+    context 'when token cannot be retrieved' do
+      let(:user    ) { double("user", id: 1234) }
+      let(:service ) { 'foo' }
+      let(:token   ) { nil   }
+      before(:each) do
+        session[:user] = user
+        session['my_token'] = 'my_secret'
+        ConsumerToken.should_receive(:find).and_return('a_token')
+        FooToken.should_receive(:access_token).and_return(token)
+        get :callback2, :id => service, :oauth_token => 'my_token'
+      end
+      it 'should redirect to the service page' do
+        response.should redirect_to oauth_consumer_url('foo')
+      end
+    end
+  end
 end
--- spec/dummy_app/config/routes.rb
+++ spec/dummy_app/config/routes.rb
@@ -4,6 +4,7 @@ DummyApp::Application.routes.draw do
   resources :oauth_consumers, :only => [:show,:destroy] do
     member do
       get :callback
+      get :callback2
     end
   end
 end
