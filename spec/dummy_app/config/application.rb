6b68112017526bb2d8f076e234ff0359bf4f52ae
Graham Davison <g.m.davison@computer.org>
2014-02-26 23:39:22 -0800

    add generators path to dummy_app

--- spec/dummy_app/config/application.rb
+++ spec/dummy_app/config/application.rb
@@ -24,6 +24,10 @@ module DummyApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = true
+    
+    generators do
+      "../../lib/generators"
+    end
   end
 end
 
