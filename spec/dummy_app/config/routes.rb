d80dc5e6e0217232065e8d059888cdf9c22faedc
Graham Davison <g.m.davison@computer.org>
2014-03-04 23:27:30 -0800

    add OAuth provider controllers, models, and routes to dummy_app

--- spec/dummy_app/config/routes.rb
+++ spec/dummy_app/config/routes.rb
@@ -1,4 +1,11 @@
 DummyApp::Application.routes.draw do
+  resources :oauth_clients
+  get   '/oauth/test_request',  :to => 'oauth#test_request',  :as => :test_request
+  get   '/oauth/token',         :to => 'oauth#token',         :as => :token
+  get   '/oauth/access_token',  :to => 'oauth#access_token',  :as => :access_token
+  get   '/oauth/request_token', :to => 'oauth#request_token', :as => :request_token
+  match '/oauth/authorize',     :to => 'oauth#authorize',     :as => :authorize,    :via => [:get, :post]
+  get   '/oauth',               :to => 'oauth#index',         :as => :oauth
   root :to => "home#index"
   
   resources :oauth_consumers, :only => [:show,:destroy] do
