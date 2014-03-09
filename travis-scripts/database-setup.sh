98838ce4bfd6eb7b52c5aa437fe43a6f77232d1c
Graham Davison <g.m.davison@computer.org>
2014-03-09 03:43:58 -0700

    make database setup more verbose

--- travis-scripts/database-setup.sh
+++ travis-scripts/database-setup.sh
@@ -5,8 +5,11 @@ DUMMY_APP_PATH="$SPEC_PATH/dummy_app"
 
 # on Rails 3, we need to explicitly initialize the test db
 RAILS_MAJOR_VERSION=`rails -v | sed -e 's/[^0-9.]//g' -e 's/^\([[:digit:]]\)\..*/\1/g'`
+echo "Rails major version: $RAILS_MAJOR_VERSION"
 if [ "$RAILS_MAJOR_VERSION" == "3" ]; then
     echo "running migration"
     cd "$DUMMY_APP_PATH"
     rake db:migrate RAILS_ENV=test
+else
+    echo "skipping migration"
 fi
