f309c1f962f017ac5a3a2ccf907ec6eebfb75756
Graham Davison <g.m.davison@computer.org>
2014-03-31 22:38:58 -0700

    use root locale files in dummy_app

--- spec/dummy_app/config/application.rb
+++ spec/dummy_app/config/application.rb
@@ -20,9 +20,7 @@ module DummyApp
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('../..', 'config/locales', '*.yml').to_s]
     config.i18n.enforce_available_locales = true
     
     generators do
