e40229b9b1e8f5357b8e83d5e5d16e3690277517
Graham Davison <g.m.davison@computer.org>
2014-04-05 22:06:20 -0700

    add test for clients_controller#update

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -7,7 +7,7 @@ describe OauthClientsController do
   describe "create" do
     let(:operation) { 'created' }
     before(:each) do
-      session[:user] = user
+      session[:user]      = user
       session['my_token'] = 'my_secret'
     end
     context 'when logged in' do
@@ -33,6 +33,59 @@ describe OauthClientsController do
         let(:save_result)         { false }
         it 'should render the new view' do
           response.should render_template :new
+        end
+      end
+    end
+  end
+  
+  describe "update" do
+    let(:operation) { 'updated' }
+    before(:each) do
+      session[:user]      = user
+      session['my_token'] = 'my_secret'
+    end
+    context 'when logged in' do
+      let(:client_applications)   { double("client_applications") }
+      let(:user       )           { double("user", id: 1234, client_applications: client_applications) }
+      let(:client_application_id) { 4321 }
+      before(:each) do
+        allow(client_applications).to receive(:find).with(client_application_id.to_s) { client_application }
+      end
+      context 'with a known client application' do
+        before(:each) do
+          expect(client_application).to receive(:update_attributes).with(nil) { update_result }
+          put :update, id: client_application_id 
+        end
+        context 'with valid client application attributes' do
+          let(:client_application ) { double("client_application", id: client_application_id) }
+          let(:update_result)       { true }
+          it 'should redirect to oauth_clients_url' do
+            response.should redirect_to action: "show", id: client_application_id
+          end
+          it 'should set the application updated flash message' do
+            expect(flash[:notice]).to eq "Updated the client information successfully"
+          end
+        end
+        context 'with invalid client application attributes' do
+          let(:client_application ) { double("client_application", id: client_application_id) }
+          let(:update_result)       { false }
+          it 'should render the edit view' do
+            response.should render_template :edit
+          end
+        end
+      end
+      context 'with an unkown client application' do
+        let(:client_application ) { nil   }
+        let(:update_result)       { false }
+        it 'raises a NotFound error' do
+          expect { put :update, id: client_application_id }.to raise_error ActiveRecord::RecordNotFound
+        end
+        it 'sets the not found flash error message' do
+          begin
+            put :update, id: client_application_id
+          rescue
+          end
+          expect(flash[:error]).to eq "Wrong application id"
         end
       end
     end
