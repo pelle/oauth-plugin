bfb9f46ee7fabb0a62ea3750fa5b30d4a30d2060
Graham Davison <g.m.davison@computer.org>
2014-04-07 22:20:45 -0700

    add tests for clients_controller#destroy

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -97,4 +97,49 @@ describe OauthClientsController do
     end
   end
   
+  describe "destroy" do
+    let(:operation) { 'destroyed' }
+    def request
+      delete :destroy, id: client_application_id
+    end
+    before(:each) do
+      session[:user]      = user
+      session['my_token'] = 'my_secret'
+    end
+    context 'when logged in' do
+      let(:client_applications)   { double("client_applications") }
+      let(:user       )           { double("user", id: 1234, client_applications: client_applications) }
+      let(:client_application_id) { 4321 }
+      before(:each) do
+        allow(client_applications).to receive(:find).with(client_application_id.to_s) { client_application }
+      end
+      context 'with a known client application' do
+        before(:each) do
+          expect(client_application).to receive(:destroy)
+          request
+        end
+        let(:client_application ) { double("client_application") }
+        it 'should redirect to oauth_clients_url' do
+          response.should redirect_to action: "index"
+        end
+        it 'should set the application registration destroyed flash message' do
+          expect(flash[:notice]).to eq "Destroyed the client application registration"
+        end
+      end
+      context 'with an unkown client application' do
+        let(:client_application ) { nil   }
+        it 'raises a NotFound error' do
+          expect { request }.to raise_error ActiveRecord::RecordNotFound
+        end
+        it 'sets the not found flash error message' do
+          begin
+            request
+          rescue
+          end
+          expect(flash[:error]).to eq "Wrong application id"
+        end
+      end
+    end
+  end
+  
 end
