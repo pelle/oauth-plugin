f1da0256c343df7e5c64be0c4815e5717219def2
Graham Davison <g.m.davison@computer.org>
2014-04-08 22:51:22 -0700

    move clients_controller strings to resource file

--- lib/generators/oauth_provider/templates/clients_controller.rb
+++ lib/generators/oauth_provider/templates/clients_controller.rb
@@ -14,7 +14,7 @@ class OauthClientsController < ApplicationController
   def create
     @client_application = current_user.client_applications.build(params[:client_application])
     if @client_application.save
-      flash[:notice] = "Registered the information successfully"
+      set_flash_message :notice, "created"
       redirect_to :action => "show", :id => @client_application.id
     else
       render :action => "new"
@@ -29,7 +29,7 @@ class OauthClientsController < ApplicationController
 
   def update
     if @client_application.update_attributes(params[:client_application])
-      flash[:notice] = "Updated the client information successfully"
+      set_flash_message :notice, "updated"
       redirect_to :action => "show", :id => @client_application.id
     else
       render :action => "edit"
@@ -38,14 +38,34 @@ class OauthClientsController < ApplicationController
 
   def destroy
     @client_application.destroy
-    flash[:notice] = "Destroyed the client application registration"
+    set_flash_message :notice, "destroyed"
     redirect_to :action => "index"
   end
+  
+  protected
+  # Sets the flash message with :key, using I18n.
+  # Example (i18n locale file):
+  #
+  #   en:
+  #     oauth_plugin:
+  #       oauth:
+  #         #messages
+  #
+  # Please refer to README or en.yml locale file to check what messages are
+  # available.
+  def set_flash_message(key, kind, options={})
+    options[:scope] = "oauth_plugin.#{controller_name}"
+    client_app_name = @client_application ? @client_application.name : ''
+    options[:client_app_name] = client_app_name
+    message = I18n.t("#{kind}", options)
+    flash[key] = message if message.present?
+  end
+  
 
   private
   def get_client_application
     unless @client_application = current_user.client_applications.find(params[:id])
-      flash.now[:error] = "Wrong application id"
+      set_flash_message :error, "error.application_not_found"
       raise ActiveRecord::RecordNotFound
     end
   end
--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -3,7 +3,7 @@ require 'dummy_app/app/controllers/application_controller'
 require 'dummy_app/app/controllers/oauth_clients_controller'
 
 describe OauthClientsController do
-  
+  let(:app_name) { 'fancy-app' }
   describe "create" do
     let(:operation) { 'created' }
     def request
@@ -19,11 +19,14 @@ describe OauthClientsController do
       before(:each) do
         expect(client_application).to receive(:save)  { save_result        }
         allow(client_applications).to receive(:build) { client_application }
-        get :create
       end
       context 'with a valid client application' do
-        let(:client_application ) { double("client_application", id: 4321) }
+        let(:client_application ) { double("client_application", id: 4321, name: app_name) }
         let(:save_result)         { true }
+        before(:each) do
+          I18n.should_receive(:t).with("#{operation}", {:scope => 'oauth_plugin.oauth_clients', :client_app_name => app_name}).and_call_original
+          request
+        end
         it 'should redirect to oauth_clients_url' do
           response.should redirect_to action: "show", id: 4321
         end
@@ -34,6 +37,9 @@ describe OauthClientsController do
       context 'with an invalid client application' do
         let(:client_application ) { double("client_application") }
         let(:save_result)         { false }
+        before(:each) do
+          request
+        end
         it 'should render the new view' do
           response.should render_template :new
         end
@@ -60,11 +66,14 @@ describe OauthClientsController do
       context 'with a known client application' do
         before(:each) do
           expect(client_application).to receive(:update_attributes).with(nil) { update_result }
-          request
         end
         context 'with valid client application attributes' do
-          let(:client_application ) { double("client_application", id: client_application_id) }
+          let(:client_application ) { double("client_application", id: client_application_id, name: app_name) }
           let(:update_result)       { true }
+          before(:each) do
+            I18n.should_receive(:t).with("#{operation}", {:scope => 'oauth_plugin.oauth_clients', :client_app_name => app_name}).and_call_original
+            request
+          end
           it 'should redirect to oauth_clients_url' do
             response.should redirect_to action: "show", id: client_application_id
           end
@@ -75,6 +84,9 @@ describe OauthClientsController do
         context 'with invalid client application attributes' do
           let(:client_application ) { double("client_application", id: client_application_id) }
           let(:update_result)       { false }
+          before(:each) do
+            request
+          end
           it 'should render the edit view' do
             response.should render_template :edit
           end
@@ -83,6 +95,9 @@ describe OauthClientsController do
       context 'with an unkown client application' do
         let(:client_application ) { nil   }
         let(:update_result)       { false }
+        before(:each) do
+          I18n.should_receive(:t).with("error.application_not_found", {:scope => 'oauth_plugin.oauth_clients', :client_app_name => ''}).and_call_original
+        end
         it 'raises a NotFound error' do
           expect { request }.to raise_error ActiveRecord::RecordNotFound
         end
@@ -116,9 +131,10 @@ describe OauthClientsController do
       context 'with a known client application' do
         before(:each) do
           expect(client_application).to receive(:destroy)
+          I18n.should_receive(:t).with("#{operation}", {:scope => 'oauth_plugin.oauth_clients', :client_app_name => app_name}).and_call_original
           request
         end
-        let(:client_application ) { double("client_application") }
+        let(:client_application ) { double("client_application", name: app_name) }
         it 'should redirect to oauth_clients_url' do
           response.should redirect_to action: "index"
         end
@@ -128,6 +144,9 @@ describe OauthClientsController do
       end
       context 'with an unkown client application' do
         let(:client_application ) { nil   }
+        before(:each) do
+          I18n.should_receive(:t).with("error.application_not_found", {:scope => 'oauth_plugin.oauth_clients', :client_app_name => ''}).and_call_original
+        end
         it 'raises a NotFound error' do
           expect { request }.to raise_error ActiveRecord::RecordNotFound
         end
